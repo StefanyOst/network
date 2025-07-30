1336e8fca1a31aff303f511d9c23c9449de21ff0
Stefany Ost <stefanyregina678@gmail.com>
2025-07-30 17:06:26 -0300

    light-mode-function

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMode() {
+  // light mode
+  const html = document.documentElement;
+
+  if (html.classList.contains("light")) {
+    html.classList.remove("light");
+  } else {
+    html.classList.add("light");
+  }
+}
